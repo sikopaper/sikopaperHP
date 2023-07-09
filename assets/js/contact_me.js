a7923a458ce2ba87c329eb4d85a7234a3265ef74
Mat Yos <info.tecmah@gmail.com>
2023-07-09 19:10:16 +0900

    version 0.0.2 update

---
+++ assets/js/contact_me.js
@@ -0,0 +1,27 @@
+---
+---
+$(function() {
+
+  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      // additional Succdess messages or events
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+  $('#success').html('');
+});
